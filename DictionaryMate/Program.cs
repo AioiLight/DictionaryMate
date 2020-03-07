afa35f9942af14ee14d7598b4cc10cb331f14ef6
AioiLight <info@aioilight.space>
2020-03-07 15:25:44 +0900

    プロジェクト ファイルを追加します。

---
+++ DictionaryMate/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AioiLight.DictionaryMate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
